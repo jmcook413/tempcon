02f408a04bd01af5c52c7829a7b3619767034b68
jmcook413 <jeremycook290@gmail.com>
2019-01-18 13:24:01 -0600

    Adds a kelvin conversion function.
    This commit adds a kelvin conversion function, and adjusts the
    fahrenheit and celsius converter functions. The two functions that have been
    changed now convert the given value to its equal on the other two
    scales.

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,42 @@ fn main() {
         .expect("Please enter number!");
 
     if scale == "f" {
-        f_to_c(value);
+        fahrenheit(value);
     } else if scale == "c" {
-        c_to_f(value);
+        celsius(value);
+    } else if scale == "k" {
+        kelvin(value);
     } else {
         println!("The first argument given must be 'c' or 'f' to specify the current scale.");
     };
 }
 
-fn f_to_c(value: i16) {
-    println!("Converting {}° fahrenheit to celsius.", value);
+fn fahrenheit(value: i16) {
+    println!("Converting {}° fahrenheit.", value);
 
-    let converted_value = ((f64::from(value) - 32.0) * (5.0 / 9.0)).round();
+    let celsius_temp = ((((f64::from(value) - 32.0) * (5.0 / 9.0)) * 100.0).round()) / 100.0;
+    let kelvin_temp = (((celsius_temp + 273.15) * 100.0).round()) / 100.0;
 
-    println!("{}° farenheit is equal to {}° celsius.", value, converted_value);
+    println!("{}° fahrenheit is equal to {}° celsius.", value, celsius_temp);
+    println!("{}° fahrenheit is equal to {}° kelvin.", value, kelvin_temp);
 }
 
-fn c_to_f(value: i16) {
-    println!("Converting {}° celsius to fahrenheit.", value);
+fn celsius(value: i16) {
+    println!("Converting {}° celsius.", value);
 
-    let converted_value = ((f64::from(value) * (9.0 / 5.0)) + 32.0).round();
+    let fahrenheit_temp = ((((f64::from(value) * (9.0 / 5.0)) + 32.0) * 100.0).round()) / 100.0;
+    let kelvin_temp = (((f64::from(value) + 273.15) * 100.0).round()) / 100.0;
 
-    println!("{}° celsius is equal to {}° farenheit .", value, converted_value);
+    println!("{}° celsius is equal to {}° fahrenheit.", value, fahrenheit_temp);
+    println!("{}° celsius is equal to {}° kelvin.", value, kelvin_temp);
+}
+
+fn kelvin(value: i16) {
+    println!("Converting {}° kelvin.", value);
+
+    let celsius_temp = (((f64::from(value) - 273.15) * 100.0).round()) / 100.0;
+    let fahrenheit_temp = ((((celsius_temp * (9.0 / 5.0)) + 32.0) * 100.0).round()) / 100.0;
+
+    println!("{}° kelvin is equal to {}° fahrenheit.", value, fahrenheit_temp);
+    println!("{}° kelvin is equal to {}° celsius.", value, celsius_temp);
 }
